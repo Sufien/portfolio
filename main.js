2815467087cdd9a0a6c16389740a62506fa95835
Soufiane <soufianetai@hotmail.it>
2022-02-23 14:57:08 +0100

    adding the cam movement on scroll (html css mod)

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 const scene =  new THREE.Scene(); 
 //adding a camera that will work as our eyes, this type of camera will actually work as an human eye
 // Args: field of view, aspect ratio, near plane, far plane
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.01, 1000);
 //object to render the scene and the camera on the DOM
 //in the element #canvas with the id:'bg'
 const renderer = new THREE.WebGLRenderer({
@@ -21,27 +21,31 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 //default camera position is on the middle of the scene, so we move it along the Z axes
 camera.position.setZ(30);
+camera.position.setX(-3);
 
 
 
 //Creating the objects in the scene
 
-//making the geometry, in this case the Torus
-const geometry = new THREE.TorusGeometry(5, 1, 16, 100);
-//we then set the materia of the object
-const material = new THREE.MeshStandardMaterial({color: 0x1F618D});
 //to make the object 'alive' we mesh the geomettry of it with the material
-const torus = new THREE.Mesh(geometry, material);
-torus.position.set(30, 15, 0);
+const torus = new THREE.Mesh(
+    //making the geometry, in this case the Torus
+    new THREE.TorusGeometry(10, 3, 16, 100),
+    //we then set the materia of the object
+    new THREE.MeshStandardMaterial({color: 0x1F618D})
+);
+torus.position.setX(15);
 scene.add(torus);
 
+
+//profile image
 //creating a box and putting an image as texture
 const sufienTexture = new THREE.TextureLoader().load('imagine_prof.jpeg');
 const image = new THREE.Mesh(
-    new THREE.BoxGeometry(5, 5, 5),
+    new THREE.BoxGeometry(3, 3, 3),
     new THREE.MeshStandardMaterial({map: sufienTexture})
 );
-image.position.set(30, 15, 0);
+image.position.setX(15);
 scene.add(image);
 
 
@@ -86,6 +90,24 @@ scene.add(pointLight);
 //this light, instead, illuminates all objects in the scene
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
 scene.add(ambientLight);
+
+
+function moveCamera(){
+    const t = document.body.getBoundingClientRect().top;
+
+    torus.rotation.x += 0.01;
+    torus.rotation.y += 0.005;
+    torus.rotation.z += 0.01;
+
+    image.rotation.y += 0.01;
+
+    camera.position.z = t * 0.8;
+    camera.rotation.y = t * 0.000000005;
+
+}
+document.body.onscroll = moveCamera;
+
+
 
 //to make the object animation we use an infinite function
 // and we render everything through the renderer
